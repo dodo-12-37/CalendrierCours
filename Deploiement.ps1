29705c09c02d9e60f3b5dc13fcc56e36741e7225
simonquillaud <quillaudsimon@outlook.com>
2023-01-15 10:13:55 -0500

    maj Script Deploiement

--- Deploiement.ps1
+++ Deploiement.ps1
@@ -1,11 +1,11 @@
 # Script de deploiement de CalendrierCours
-# Version 1.0
+# Version 2.0
 # Simon Quillaud
-# Decembre 2022
+# Janvier 2023
 
 $chemin= Get-Location
 
 dotnet restore $chemin\CalendrierCours.sln
 dotnet build $chemin\CalendrierCours.sln --configuration Release
 
-dotnet publish $chemin\CalendrierCours.ConsoleUI --configuration Release --output $chemin\CalendrierCours.Publish
+dotnet publish $chemin\CalendrierCours.WinFormUI --configuration Release --output $chemin\CalendrierCours.Publish
